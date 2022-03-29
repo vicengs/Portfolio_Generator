6f2e30d1f5ec4b1470794c07be232c81e6422c8e
Vicente Garcia <vicen1@hotmail.com>
2022-03-29 16:59:11 -0500

    Add inquirer function

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const fs = require("fs");
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+/*const fs = require("fs");
 const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-fs.writeFile('index.html', generatePage(name, github), err => {
+const pageHTML = generatePage(name, github);
+fs.writeFile('index.html', pageHTML, err => {
 if (err) throw err;
 console.log('Portfolio complete! Check out index.html to see the output!');
-});
+});*/
